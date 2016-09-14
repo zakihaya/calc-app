ddfbc9e19249efc85f182adfcb698260cee8329f
hayazaki <toru.hayazaki@gmail.com>
2016-09-15 00:50:08 +0900

    Initial Commit

---
+++ MyCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCalc
+//
+//  Created by hayazaki on 2016/09/15.
+//  Copyright © 2016年 hayazaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
